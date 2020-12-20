c42d1021a1fbc1d3af78b19e06bd99e890bae1af
Battleaxe76 <70934717+Battleaxe76@users.noreply.github.com>
2020-12-20 11:28:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,11 +30,49 @@ function gotPoses(results) {
         scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreRightwrist=" + scoreRightWrist + "scoreLeftwrist=" + scoreLeftWrist);
-        rightWristX = results[0].pose.rightWrist.x
-        rightWristY = results[0].pose.rightWrist.y
-        leftWristX = results[0].pose.leftWrist.x
-        leftWristY = results[0].pose.leftWrist.y
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
         console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
         console.log("leftWristX=" + leftWristX + "leftWristY=" + leftWristY);
     }
+}
+function draw(){
+    image(video,0,0,600,500);
+    fill("maroon");
+    stroke("lightblue");
+if(scoreRightWrist>0.2){
+    circle(rightWristX,rightWristY,20);
+    if(rightWristY>0&&rightWristY<=100){
+        document.getElementById("speed").innerHTML="speed=0.5x";
+        song.rate(0.5);
+    }
+
+    else if(rightWristY>100&&rightWristY<=200){
+        document.getElementById("speed").innerHTML="speed=1.0x";
+        song.rate(1);
+    }
+    else if(rightWristY>200&&rightWristY<=300){
+        document.getElementById("speed").innerHTML="speed=1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300&&rightWristY<=400){
+        document.getElementById("speed").innerHTML="speed=2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400){
+        document.getElementById("speed").innerHTML="speed=2.5x";
+        song.rate(2.5);
+    }
+}
+if(scoreLeftWrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals=floor(InNumberleftWristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML="volume="+volume;
+    song.setVolume(volume);
+
+}
 }
